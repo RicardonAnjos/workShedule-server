6a1529d30c027a739eb0afab9366d1d9eb1dac9b
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-10 20:19:57 -0300

    implementa rota put e adciona async await

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,37 @@ app.use(bodyParser.json());
 
 const prisma = new PrismaClient();
 
-app.get('/schedule', (_req, res) => {
-  prisma.workSchedule
-    .findMany({
+app.get('/schedule', async (_req, res) => {
+  try {
+    const data = await prisma.workSchedule.findMany({
       include: {
         days: true,
       },
-    })
-    .then((data) => res.json(data));
+    });
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to fetch work schedules.' });
+  }
 });
+
+
+app.put('/schedule/:id', async (req, res) => {
+  const { id } = req.params;
+  const { startTime, endTime } = req.body;
+
+  try {
+    await prisma.workSchedule.update({
+      where: { id },
+      data: { startTime, endTime },
+    });
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Failed to update work schedule.' });
+  }
+});
+
 
 app.listen(3333, () => {
   console.log('Server started on port 3333!');
