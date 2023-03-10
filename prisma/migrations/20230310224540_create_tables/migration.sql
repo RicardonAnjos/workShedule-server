06a66ec6a05b3db627a08469741ade72d9836dfd
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-10 19:50:17 -0300

    alteração no schma weekday day = Int

--- prisma/migrations/20230310224540_create_tables/migration.sql
+++ prisma/migrations/20230310224540_create_tables/migration.sql
@@ -8,7 +8,7 @@ CREATE TABLE "WorkSchedule" (
 -- CreateTable
 CREATE TABLE "WeekDay" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "day" TEXT NOT NULL,
+    "day" INTEGER NOT NULL,
     "scheduleId" TEXT NOT NULL,
     CONSTRAINT "WeekDay_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "WorkSchedule" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
