baba4c2bbdb0330c72ea257a845e90f84a1db5cd
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-10 18:35:18 -0300

    cria models e mogrations com prisma

---
+++ prisma/migrations/20230310213447_create_tables/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "WorkSchedule" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "startTime" TEXT NOT NULL,
+    "endTime" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "WeekDay" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day" TEXT NOT NULL,
+    "scheduleId" TEXT NOT NULL,
+    CONSTRAINT "WeekDay_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "WorkSchedule" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
