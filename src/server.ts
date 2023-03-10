a8094d11126e350b2f8b882c6fe4c9ee36513a9c
Ricardo Anjos <ricardoanj.14@gmail.com>
2023-03-10 19:18:36 -0300

    instala dependencias de tipo body-parser e ts-node

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,14 @@ app.use(bodyParser.json());
 
 const prisma = new PrismaClient();
 
-app.get('/', (_req, res) => {
-  return res.json({ message: 'Hello World' });
+app.get('/schedule', (_req, res) => {
+  prisma.workSchedule
+    .findMany({
+      include: {
+        days: true,
+      },
+    })
+    .then((schedule) => res.json(schedule));
 });
 
 app.listen(3333, () => {
